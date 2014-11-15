b59765ffbceba4566e6ab1d1e714e7fad1e28d16
radex <this.is@radex.io>
2014-11-15 14:09:49 +0100

    Initial commit

---
+++ Goldengate/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Goldengate
+//
+//  Created by Radosław Pietruszewski on 15/11/14.
+//  Copyright (c) 2014 Radosław Pietruszewski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    }
+
+
+}
+
---
+++ Goldengate/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Goldengate
+//
+//  Created by Radosław Pietruszewski on 15/11/14.
+//  Copyright (c) 2014 Radosław Pietruszewski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
