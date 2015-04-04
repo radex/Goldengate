2287be84cecdaa30b5e1f39242764c6ba4be5873
radex <this.is@radex.io>
2015-04-04 22:58:47 +0200

    Taylor + Goldengate PoC

--- Goldengate/AppDelegate.swift
+++ Goldengate/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Cocoa
 import WebKit
+import Taylor
 
 // There are some bugs with SourceKit and weird shit happens in this file, so I'm putting the playground code in a separate file
 
@@ -8,7 +9,24 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     @IBOutlet weak var contentView: NSView!
     
+    let taylor = Taylor.Server()
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // set up Taylor
+        
+        taylor.addHandler(Middleware.staticDirectory("", directory: "/Users/radex/Desktop/prodmag"))
+        
+        let port = 3001
+        taylor.startListening(port: port, forever: false) { result in
+            switch result {
+            case .Success:
+                println("Up and running!")
+            case .Error(let e):
+                println("Server start failed \(e)")
+            }
+        }
+        
+        // set up Goldengate
         let goldengate = Goldengate()
         goldengate.addPlugins(ReadLaterPlugin(), OtherPlugin())
         
@@ -18,6 +36,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         webView.autoresizingMask = .ViewWidthSizable | .ViewHeightSizable
         contentView.addSubview(webView)
         
-        goldengate.vc.loadURL("http://goldengate.dev")
+        goldengate.vc.loadURL("http://localhost:3001")
     }
 }
